e2edafd9672d88a98ce9a9b9c71a222c6b8b3928
Azehra2 <zhrgyv21@gmail.com>
2022-11-28 09:55:43 +0400

    resturcture

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
+import Button from "./components/Button";
+import Heading from "./components/Heading";
 
-let name = 'ali'
-
-const h2Styles = {
-  backgroundColor: 'black',
-  color: 'lime',
-  textAlign: 'center'
-}
 const divStyles = {
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
   gap: 30
 }
-const btnStyles = {
-  backgroundColor: 'black',
-  color: 'lime',
-  textAlign: 'center',
-  border: '1px solid lime',
-  borderRadius: 4,
-  cursor: 'pointer'
-}
+
 
 function App() {
   return (
     <div style={divStyles}>
-      <h1 style={h2Styles}>Ay {name}, 2+2={2 + 2} edir.</h1>
-      <button
-        onClick={() => {
-          alert('test')
-        }}
-        style={btnStyles}
-      >click me</button>
+      <Heading />
+      <Button />
     </div>
   );
 }
---
+++ src/components/Heading/index.js
@@ -0,0 +1,13 @@
+const h2Styles = {
+    backgroundColor: 'black',
+    color: 'lime',
+    textAlign: 'center'
+}
+
+function Heading() {
+    return (
+        <h1 style={h2Styles}>2+2={2 + 2} edir.</h1>
+    )
+}
+
+export default Heading;
